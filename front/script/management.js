1d30f119022cbd0d4f03f73636dcf645aac3bc05
CORP\hledall <ledall.hugo@gmail.com>
2017-04-13 10:29:50 +0200

    Modif 2 page -> 2 onglets

--- front/script/management.js
+++ front/script/management.js
@@ -15,7 +15,18 @@
 	}
 })();
 
-
+/*
+ * global method for adding moods count
+ */
+(function() {
+	var uuid = window.location.search.substring(1).split("&")[0].substring(5);
+	var url = "http://localhost:8080/api-admin/projects/" + uuid + "/moods";
+	$.get(url).done(function(data) {
+		if (data.length != 0) {			
+			makeStats(data);			
+		}
+	});
+})();
 /*
  * global method for adding table
  */
@@ -42,7 +53,7 @@
  * getting data for each days
  */
 function eachDaysMoods(data){	
-	var values = [0,0,0,0,0]
+	var values = [0,0,0,0,0];
 	for (var i = 0; i < data.length; i++) {
 		var listList=[[],[],[],[],[]];
 		createRow(data,i);
@@ -163,5 +174,105 @@ function createDiv(value ,index){
 	clone.appendChild(document.createTextNode(value))
 	document.getElementById("count").appendChild(clone);
 }
+
+
+/*
+ * get data for each moods
+ */
+function makeStats(data){	
+	var values = [0,0,0,0,0];
+	var listCom = [];
+	var avg = 0;
+	var total = 0;
+	for (var i = 0; i < data.length; i++) {
+		for (var y = 0; y < data[i].length; y++) {
+			values[data[i][y].mood] ++;	
+			if(null != data[i][y].comment && null != data[i][y].date){
+				listCom.push({"date" :data[i][y].date , "comment" : data[i][y].comment});
+			}
+		}
+	}
+	for(var v = 4;v>=0;v--){
+		total += values[v];
+		avg += (values[v]*v);
+	}
+	console.log(listCom);
+	avg = avg/total;
+	createDivBefore("Average mood : "+Math.trunc(avg),Math.trunc(avg));
+	useC3(values);
+	initCommentTable(listCom);
+	
+}
+/*
+ * clone the mood's div
+ */
+function createDivBefore(value ,index){
+	var clone = document.getElementById("countDuplicate").cloneNode(true);
+	clone.setAttribute("id","count"+index);
+	clone.appendChild(document.createTextNode(value))
+	document.getElementById("stats").insertBefore(clone,document.getElementById("stats").firstChild);
+}
+
+
+function useC3(values){
+	var chart = c3.generate({
+		bindto:'#graphs',	
+		 size: {
+	            height: 650,
+	            width: 650
+		},
+	    data: {	    	    
+	        columns: [
+	            ['Deplorable', values[0]],
+	            ['Bad', values[1]],
+	            ['Average', values[2]],
+	            ['Good', values[3]],
+	            ['Awesome', values[4]]
+	        ],
+	       
+	        colors: {
+	            Deplorable: '#ED1C24',
+	            Bad: '#FF7F27',
+	            Average: '#FFF200',
+	            Good: '#00A2E8',
+	 	        Awesome: '#22B14C',
+	        },
+	        type : 'donut',
+	        onclick: function (d, i) { console.log("onclick", d, i); },
+	        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+	        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+	    },
+	    donut: {
+	    	width:180,
+	        title: "My day was :"
+	    }
+	});
+}
+
+function initCommentTable(data){
+	var table =  $('#tabComments').DataTable({
+		"data" : data,
+		"columns" : [ {
+			"data" : "date"
+		}, {
+			"data" : "comment"
+		} ]
+	});
+}
+/*
+ * listener for deleting a project in the DB
+ */
+$('#moodsBtn').click( function () {
+	document.getElementById("stats").style.display = "none";
+	document.getElementById("graphs").style.display = "none";
+	document.getElementById("moods").style.display = "inline-block";
+	document.getElementById("count").style.display = "inline-block";
+});
+$('#statsBtn').click( function () {
+	document.getElementById("moods").style.display = "none";
+	document.getElementById("count").style.display = "none";
+	document.getElementById("stats").style.display = "inline-block";
+	document.getElementById("graphs").style.display = "inline-block";
+});
 
 
